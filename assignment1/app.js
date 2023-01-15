c06cb576d5e5cef10ba8dcf695362fe5a5cdc4bf
Aman Malik <72318758+Amanster23@users.noreply.github.com>
2023-01-15 11:14:58 -0600

    Create app.js

---
+++ assignment1/app.js
@@ -0,0 +1,14 @@
+// Displays 'Hello world!'
+//
+// References:
+//   https://repl.it/languages/express
+//   https://expressjs.com/en/starter/hello-world.html
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello Aman!');
+});
+
+app.listen(port=3000);
