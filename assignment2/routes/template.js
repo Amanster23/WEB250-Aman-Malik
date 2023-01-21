869b79698785ddad9afa1d3c5b1e881bcc1b63ab
Aman Malik <72318758+Amanster23@users.noreply.github.com>
2023-01-21 13:50:56 -0600

    Create template.js

---
+++ assignment2/routes/template.js
@@ -0,0 +1,25 @@
+// This module returns a rendered template.
+//
+// References:
+//  https://expressjs.com/en/starter/hello-world.html
+//  https://expressjs.com/en/guide/routing.html
+//  https://www.npmjs.com/package/handlebars
+//  https://handlebarsjs.com/guide
+
+const express = require('express')
+const fs = require("fs");
+const handlebars = require('handlebars');
+
+const router = express.Router()
+router.get("/", function (request, response) {
+    let source = fs.readFileSync("./templates/template.html");
+    let template = handlebars.compile(source.toString());
+    let data = {
+        greeting: "Hello",
+        name: "world"
+    }
+    let result = template(data);
+    response.send(result);
+});
+
+module.exports = router;
