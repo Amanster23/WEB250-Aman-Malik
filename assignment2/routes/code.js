dd0f6a02f0fe0fbe9e50757576f596cbe8c80b17
Aman Malik <72318758+Amanster23@users.noreply.github.com>
2023-01-21 13:50:35 -0600

    Create code.js

---
+++ assignment2/routes/code.js
@@ -0,0 +1,24 @@
+// Displays "Hello code world!"
+//
+// References:
+//  https://expressjs.com/en/starter/hello-world.html
+//  https://expressjs.com/en/guide/routing.html
+
+const express = require('express')
+const router = express.Router()
+
+const HTML = `
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Hello World</title>
+<link rel="stylesheet" href="styles.css">    
+`
+
+router.get("/", function (request, response) {
+    result = HTML;
+    result += "Hello code world!";
+    response.send(result);
+});
+
+module.exports = router;
