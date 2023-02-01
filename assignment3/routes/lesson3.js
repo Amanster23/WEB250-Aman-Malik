bcd9b2b5ced74aee1d9d2d15a0b328993c6013cf
Aman Malik <72318758+Amanster23@users.noreply.github.com>
2023-01-31 21:55:49 -0600

    Update lesson3.js

--- assignment3/routes/lesson3.js
+++ assignment3/routes/lesson3.js
@@ -14,52 +14,34 @@ const handlebars = require('handlebars');
 const router = express.Router()
 
 router.get("/", function (request, response) {
-    let result = "";
-    if (request.query.feet) {
-      let feet = request.query.feet;
-      let yards = feet / 3;
-      let inches = feet * 12;
-      result = feet + " feet is " + yards + " yards and " + inches + " inches.";
-    } else if (request.query.inches) {
-      let inches = request.query.inches;
-      let feet = inches / 12;
-      let yards = inches / 36;
-      result = inches + " inches is " + feet + " feet and " + yards + " yards ";
-    }
-  
-    let source = fs.readFileSync("./templates/lesson3.html");
-    let template = handlebars.compile(source.toString());
-    let data = {
-      feet: result,
-      yards: "",
-      inches: "",
-    };
-    result = template(data);
-    response.send(result);
-  });
+  let result = "";
+  if (request.query.feet) {
+    let feet = request.query.feet;
+    let yards = feet / 3;
+    let inches = feet * 12;
+    result = feet + " feet is " + yards + " yards and " + inches + " inches.";
+  } else if (request.query.inches) {
+    let inches = request.query.inches;
+    let feet = inches / 12;
+    let yards = inches / 36;
+    result = inches + " inches is " + feet + " feet and " + yards + " yards ";
+  } else if (request.query.yards) {
+    let yards = request.query.yards;
+    let feet = yards * 3;
+    let inches = yards * 36;
+    result = yards + " yards is " +
+      feet + " feet and " + inches + " inches.";
+  }
 
-
-
-router.post("/", function (request, response) {
-    let result = "";
-
-    if (request.body.yards) {
-        let yards = request.body.yards;
-        let feet = yards * 3;
-        let inches = yards * 36;
-        result = yards + " yards is " +
-            feet + " feet and " + inches + " inches.";
-    }
-
-    let source = fs.readFileSync("./templates/lesson3.html");
-    let template = handlebars.compile(source.toString());
-    let data = {
-        feet: "",
-        inches: "",
-        yards: result
-    }
-    result = template(data);
-    response.send(result);
+  let source = fs.readFileSync("./templates/lesson3.html");
+  let template = handlebars.compile(source.toString());
+  let data = {
+    feet: result,
+    yards: "",
+    inches: "",
+  };
+  result = template(data);
+  response.send(result);
 });
 
 module.exports = router;
